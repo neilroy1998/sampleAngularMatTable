d5a26eb3036267230da8abefc5fd5753914fa5ab
rajit.roy <rajit.roy@wabtec.com>
2021-08-03 03:13:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,109 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
+import {MatSort} from '@angular/material/sort';
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MatTable, MatTableDataSource} from "@angular/material/table";
+
+interface schoolMarks {
+	name: String,
+	subject: String,
+	marks: number,
+	passed: boolean
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myReactiveTable1';
+export class AppComponent implements OnInit{
+
+	@ViewChild(MatSort) sort: MatSort;
+	@ViewChild('matTableSchool') matTableSchool: MatTable<any>;
+
+	data: schoolMarks[];
+	tableData: MatTableDataSource<any>;
+
+	displayedCols: String[] = [
+		"action",
+		"name",
+		"subject",
+		"marks",
+		"passed"
+	];
+
+	tableFormGroup: FormGroup;
+
+	constructor(private fb: FormBuilder,
+				private cdr: ChangeDetectorRef) {
+	}
+
+	getData() {
+		return fetch("../assets/data/data.json").then(res => res.json()).then((res: schoolMarks[]) => {
+			this.data = res;
+		})
+	}
+
+	any2Int(ctrl: any): schoolMarks {
+		return ctrl as schoolMarks;
+	}
+
+	logger(data: schoolMarks) {
+		console.log(data);
+	}
+
+	ngOnInit() {
+		this.tableFormGroup = this.fb.group({
+			rows: this.fb.array([])
+		});
+
+		this.getData().then((res) => {
+			console.log(this.data);
+
+			this.setRows();
+
+			for (let i=0; i<this.data.length; i++) {
+				this.tableFormGroup.get("rows").get(`${i}`).valueChanges.subscribe((newVal) => {
+					console.log(newVal);
+				});
+			}
+		});
+	}
+
+	setRows() {
+		const formCtrl = this.tableFormGroup.get('rows') as FormArray;
+		this.data.forEach((d) => {
+			formCtrl.push(this.getRows(d));
+		});
+		this.tableData = new MatTableDataSource<any>(this.data);
+		this.tableData.sort = this.sort;
+	}
+
+	getRows(row: schoolMarks): FormGroup {
+		return this.fb.group({
+			name: [row ? row.name : "", Validators.required],
+			subject: [row ? row.subject : ""],
+			marks: [row ? row.marks : 0, Validators.min(40)],
+			passed: [row ? row.passed : false, Validators.requiredTrue],
+		})
+	}
+
+	addRow() {
+		const formCtrl = this.tableFormGroup.get('rows') as FormArray;
+		formCtrl.push(this.getRows(null));
+		this.tableData.data.push(this.getRows(null).value);
+		this.tableData = new MatTableDataSource<any>(this.data);
+
+		console.log(this.tableFormGroup.value)
+
+		for (let i = 0; i < this.tableFormGroup.value.length; i++) {
+			this.tableFormGroup.get("rows").get(`${i}`).valueChanges.subscribe((newVal) => {
+				console.log(newVal);
+			});
+		}
+	}
+
+	logData() {
+		console.log(this.tableData);
+		console.log(this.tableFormGroup.value);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {ReactiveFormsModule} from "@angular/forms";
+import { MatTableModule } from "@angular/material/table";
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatSortModule} from "@angular/material/sort";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+	imports: [
+		BrowserModule,
+		ReactiveFormsModule,
+		BrowserAnimationsModule,
+		MatTableModule,
+		MatButtonModule,
+		MatSortModule
+	],
+  providers: [
+  	MatTableModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
